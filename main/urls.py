16fa562602729b964b5d5e46c01a3ea6d76d6e18
chutapelu <juanlinaresc89@gmail.com>
2024-09-17 09:20:15 -0300

    [ADD] Se agrega funcion editar y eliminar propiedad y el template editar,

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.urls import path
-from main.views import index, register, profile,change_pass, add_propiedad
+from main.views import index, register, profile,change_pass, add_propiedad, edit_propiedad, delete_propiedad
 
 urlpatterns = [
     path('', index, name='index'),
@@ -8,4 +8,6 @@ urlpatterns = [
     path('accounts/profile/', profile, name='profile',),
     path('accounts/change-pass/', change_pass, name='change_pass',),
     path('propiedad/add-propiedad/', add_propiedad, name='add_propiedad',),
+    path('propiedad/edit-propiedad/<id>', edit_propiedad, name='edit_propiedad',),
+    path('propiedad/delete-propiedad/<id>', delete_propiedad, name='delete_propiedad',),
 ]
