1015ef308393af8e6768df676373b4d2df09e055
chutapelu <juanlinaresc89@gmail.com>
2024-09-17 00:52:25 -0300

    [ADD] Se creo index.html y se modifico  navbar y footer

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from django.urls import path
+from main.views import index, register
 
 urlpatterns = [
-    
+    path('', index, name='index'),
+    path('accounts/register/', register, name='register',),
 ]
